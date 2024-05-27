0f752eecc1b8182f02d551c367c95639ccb4af30
Oxicid <Oxicid2019@gmail.com>
2024-05-28 01:40:49 +0300

    Add functions: inv_lerp and remap

--- utils/umath.py
+++ utils/umath.py
@@ -1,4 +1,5 @@
 import math
+from bl_math import lerp
 from mathutils import Vector
 
 
@@ -7,3 +8,25 @@ def vec_isclose_to_uniform(delta: Vector, abs_tol: float = 0.00001):
 
 def vec_isclose_to_zero(delta: Vector, abs_tol: float = 0.00001):
     return math.isclose(delta.x, 0, abs_tol=abs_tol) and math.isclose(delta.y, 0, abs_tol=abs_tol)
+
+
+# https://gist.github.com/laundmo/b224b1f4c8ef6ca5fe47e132c8deab56
+def inv_lerp(a: float, b: float, v: float) -> float:
+    """Inverse Linar Interpolation, get the fraction between a and b on which v resides.
+    Examples
+    --------
+        0.5 == inv_lerp(0, 100, 50)
+        0.8 == inv_lerp(1, 5, 4.2)
+    """
+    return (v - a) / (b - a)
+
+def remap(i_min: float, i_max: float, o_min: float, o_max: float, v: float) -> float:
+    """Remap values from one linear scale to another, a combination of lerp and inv_lerp.
+    i_min and i_max are the scale on which the original value resides,
+    o_min and o_max are the scale to which it should be mapped.
+    Examples
+    --------
+        45 == remap(0, 100, 40, 50, 50)
+        6.2 == remap(1, 5, 3, 7, 4.2)
+    """
+    return lerp(o_min, o_max, inv_lerp(i_min, i_max, v))
