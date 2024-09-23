a8dfc68d84c4f89404b1a937b45e709df3f41d36
Oxicid <Oxicid2019@gmail.com>
2024-09-23 17:38:12 +0300

    Restructuring buttons for Align OT

--- ui.py
+++ ui.py
@@ -5,9 +5,42 @@ from bpy.types import Panel
 
 class UNIV_PT_General(Panel):
     bl_label = "UniV"
+    bl_idname = 'UNIV_PT_General'
     bl_space_type = 'IMAGE_EDITOR'
     bl_region_type = 'UI'
     bl_category = "UniV"
+
+    @staticmethod
+    def draw_align_buttons(where, *, alignment='EXPAND', scale_x=1.0):
+        def ly_wide_text_op(layout, direction, *, text):
+            row = layout.row(align=True)
+            row.operator('uv.univ_align', text=text).direction = direction
+
+        def ly_mid_mid_text_op(layout, direction, *, text, opt='uv.univ_align'):
+            row = layout.row(align=True)
+            row.operator(opt, text=text).direction = direction
+
+        where.alignment = alignment
+        colMain = where.column(align=True)
+        colMain.scale_x = scale_x
+        rowTop = colMain.row(align=True)
+
+        ly_wide_text_op(rowTop, 'LEFT_UPPER', text='↖')
+        ly_wide_text_op(rowTop.row(), 'UPPER', text='↑')
+        ly_wide_text_op(rowTop, 'RIGHT_UPPER', text='↗')
+        ##
+        rowMiddle = colMain.row().row(align=True)
+        ly_mid_mid_text_op(rowMiddle, 'LEFT', text='← ')
+        rowMidMiddle = rowMiddle.row().row(align=True)
+        ly_mid_mid_text_op(rowMidMiddle, 'HORIZONTAL', text='—')
+        ly_mid_mid_text_op(rowMidMiddle, 'CENTER', text='+')
+        ly_mid_mid_text_op(rowMidMiddle, 'VERTICAL', text='|')
+        ly_mid_mid_text_op(rowMiddle, 'RIGHT', text=' →')
+        ##
+        rowBottom = colMain.row(align=True)
+        ly_wide_text_op(rowBottom, 'LEFT_BOTTOM', text='↙')
+        ly_wide_text_op(rowBottom.row(), 'BOTTOM', text='↓')
+        ly_wide_text_op(rowBottom, 'RIGHT_BOTTOM', text='↘')
 
     def draw(self, context):
         layout = self.layout
@@ -33,34 +66,12 @@ class UNIV_PT_General(Panel):
         row.operator('uv.univ_orient', text='→').edge_dir = 'HORIZONTAL'
         row.operator('uv.univ_orient', text='↑').edge_dir = 'VERTICAL'
 
+        col_for_align = col.column()
+        col_for_align.separator(factor=0.5)
+        self.draw_align_buttons(col_for_align)
+
+        col = layout.column()
         col_align = col.column(align=True)
-        col_align.separator(factor=0.35)
-        row = col_align.row(align=True)
-        col = row.column(align=True)
-        col.operator('uv.univ_align', text='↖').direction = 'LEFT_UPPER'
-        col.operator('uv.univ_align', text='← ').direction = 'LEFT'
-        col.operator('uv.univ_align', text='↙').direction = 'LEFT_BOTTOM'
-
-        col = row.column(align=True)
-        col.operator('uv.univ_align', text='↑').direction = 'UPPER'
-        col.operator('uv.univ_align', text='+').direction = 'CENTER'
-        col.operator('uv.univ_align', text='↓').direction = 'BOTTOM'
-
-        col = row.column(align=True)
-        col.operator('uv.univ_align', text='↗').direction = 'RIGHT_UPPER'
-        col.operator('uv.univ_align', text=' →').direction = 'RIGHT'
-        col.operator('uv.univ_align', text='↘').direction = 'RIGHT_BOTTOM'
-
-        row_tr = col_align.row(align=True)
-        col = row_tr.column(align=True)
-
-        row = col.row(align=True)
-        row.operator('uv.univ_align', text='—').direction = 'HORIZONTAL'
-        row.operator('uv.univ_align', text='|').direction = 'VERTICAL'
-
-        col_align = col.column(align=True)
-        col_align.separator(factor=0.35)
-
         split = col_align.split(align=True)
         row = split.row(align=True)
         row.operator('uv.univ_rotate', text='Rotate')
@@ -144,6 +155,44 @@ class UNIV_PT_General(Panel):
 
         split = col_align.split(align=True)
         split.operator('uv.univ_cut', text='Cut')
+
+    @staticmethod
+    def __draw_align_buttons(where, *, alignment='CENTER', scale_x=1.0):
+        def ly_wide_icon_op(layout, *, icon):
+            row = layout.row(align=True)
+            row.ui_units_x = 3
+            row.scale_x = 2.05
+            row.operator('uv.univ_align', text="", icon=icon)
+
+        def ly_mid_mid_op(layout, *, icon):
+            row = layout.row(align=True)
+            row.operator('uv.univ_align', text="", icon=icon)
+            row.scale_x = 1.5
+
+        colMain = where.column(align=True)
+        colMain.scale_x = scale_x
+        rowTop = colMain.row(align=True)
+        rowTop.alignment = alignment
+        ly_wide_icon_op(rowTop, icon='RESTRICT_SELECT_ON')
+        ly_wide_icon_op(rowTop.row(), icon='SORT_DESC')
+        ly_wide_icon_op(rowTop, icon='CURVE_PATH')
+        ##
+        rowMiddle = colMain.row().row(align=True)
+        rowMiddle.alignment = alignment
+        ly_wide_icon_op(rowMiddle, icon='BACK')
+
+        rowMidMiddle = rowMiddle.row().row(align=True)
+        ly_mid_mid_op(rowMidMiddle, icon='REMOVE')
+        ly_mid_mid_op(rowMidMiddle.row(), icon='ADD')
+        ly_mid_mid_op(rowMidMiddle.row(), icon='THREE_DOTS')
+
+        ly_wide_icon_op(rowMiddle, icon='FORWARD')
+        ##
+        rowBottom = colMain.row(align=True)
+        rowBottom.alignment = alignment
+        ly_wide_icon_op(rowBottom, icon='LINE_DATA')
+        ly_wide_icon_op(rowBottom.row(), icon='SORT_ASC')
+        ly_wide_icon_op(rowBottom, icon='DECORATE_LIBRARY_OVERRIDE')
 
 class UNIV_PT_General_VIEW_3D(Panel):
     bl_label = "UniV"
