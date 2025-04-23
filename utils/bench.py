b5227f5a9caaa9648350731e5e88440b9989692d
Oxicid <Oxicid2019@gmail.com>
2025-04-23 04:12:12 +0300

    Improve benchmark

--- utils/bench.py
+++ utils/bench.py
@@ -14,36 +14,60 @@ from dataclasses import dataclass
 
 @dataclass
 class timer:
+    repeats: int = 1
     text: str = ''
-    repeat: int = 1
-    name = ''
 
     def __enter__(self):
-        self._start_time = time.perf_counter()
+        self._fix_args()
+        if self.repeats != 1:
+            raise ValueError('Context t manager not support repeats')
+        self._start = time.perf_counter()
 
     def __exit__(self, *exc_info):
-        elapsed_time = (time.perf_counter() - self._start_time)
-
-        prefix = ''
-        if self.name:
-            prefix = self.name + '. '
-        if self.text:
-            prefix += self.text + '. '
-
-        if self.repeat > 1 and self.name:
-            print(f"{prefix}Total time: {elapsed_time:0.4f}. Avg time: {(elapsed_time / self.repeat):0.4f} seconds")
-        else:
-            print(f"{prefix}Elapsed time: {elapsed_time:0.4f} seconds")
+        self._print_time_info()
 
     def __call__(self, func):
         @functools.wraps(func)
         def wrapper_timer(*args, **kwargs):
-            self.name = func.__name__
-            with self:
-                for i in range(self.repeat):
-                    result = func(*args, **kwargs)
-                return result
+            self._fix_args()
+            if self.text:
+                self.text = f'{self.text} {func.__name__}'
+            else:
+                self.text = func.__name__
+
+            self._start = time.perf_counter()
+            result = None
+            for i in range(self.repeats):
+                result = func(*args, **kwargs)
+            self._print_time_info()
+            return result
         return wrapper_timer
+
+    def __iter__(self):
+        self._fix_args()
+        self._start = time.perf_counter()
+        for _ in range(self.repeats):
+            yield
+        self._print_time_info()
+
+    def _fix_args(self):
+        if isinstance(self.repeats, str):
+            old_name = self.text
+            self.text = self.repeats
+            self.repeats = old_name if isinstance(old_name, int) else 1
+
+        if self.repeats < 1:
+            raise ValueError(f"Expected 'repeats' to be at least 1, but got {self.repeats}")
+
+    def _print_time_info(self):
+        elapsed_time = (time.perf_counter() - self._start)
+
+        if self.text:
+            self.text += '. '
+        if self.repeats > 1:
+            print(f"{self.text}Total time: {elapsed_time:0.4f}. Avg: {(elapsed_time / self.repeats):0.4f}")
+        else:
+            print(f"{self.text}Elapsed time: {elapsed_time:0.4f} seconds")
 
 def profile(func):
     # Source: https://osf.io/upav8
